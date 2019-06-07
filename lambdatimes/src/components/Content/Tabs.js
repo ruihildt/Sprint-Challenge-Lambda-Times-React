b0000fa1fcaf8b1a4eb5dd31e794e0f4d9510cb1
ruihildt <ruihildt@protonmail.com>
2019-06-07 10:42:58 +0200

    Implement Tabs and Tab components

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Tab from './Tab';
+import pt from 'prop-types';
+// import { tabData } from '../../data';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,6 +10,15 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {
+              props.tabs.map(tab =>
+                <Tab
+                tab={tab}
+                selectedTab={props.selectedTab}
+                selectTabHandler={props.selectTabHandler}
+                />
+              )
+            }
       </div>
     </div>
   );
@@ -14,3 +26,7 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+Tabs.propTypes = {
+  tabs: pt.arrayOf(pt.string).isRequired,
+}
